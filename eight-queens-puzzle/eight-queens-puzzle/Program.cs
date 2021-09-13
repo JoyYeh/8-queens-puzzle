a71efde938eb931fd83562eff3292bb75f995f29
PHOENIX-5\Joy <joy@phoenix-5.com>
2021-09-13 21:28:44 +0800

    Initial commit

---
+++ eight-queens-puzzle/eight-queens-puzzle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eight_queens_puzzle
+{
+    class Program
+    {
+        const int QSize = 8; //Queen Count
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eight-queens-puzzle");
+        }
+    }
+}
